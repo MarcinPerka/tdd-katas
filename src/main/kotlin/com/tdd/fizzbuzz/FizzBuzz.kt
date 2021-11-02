f453e48b06665b67ac1dfb4a96149ea34b79d73a
MarcinPerka <marcinp1596@gmail.com>
2021-11-02 17:53:46 +0100

    WIP FizzBuzz

--- src/main/kotlin/com/tdd/fizzbuzz/FizzBuzz.kt
+++ src/main/kotlin/com/tdd/fizzbuzz/FizzBuzz.kt
@@ -3,19 +3,35 @@ package com.tdd.fizzbuzz
 class FizzBuzz {
 
     companion object {
-        private const val FIZZ = "Fizz"
-        private const val BUZZ = "Buzz"
-        private const val FIZZ_BUZZ = "$FIZZ$BUZZ"
+        const val FIZZ = "Fizz"
+        const val BUZZ = "Buzz"
+        const val FIZZ_BUZZ = "$FIZZ$BUZZ"
+        const val FOO = "Foo"
+        const val BOO = "Boo"
+        const val FOO_BOO = "$FOO$BOO"
+        const val FIZZ_FOO = "$FIZZ$FOO"
+        const val FIZZ_BOO = "$FIZZ$BOO"
+        const val BUZZ_FOO = "$BUZZ$FOO"
+        const val BUZZ_BOO = "$BUZZ$BOO"
     }
+
+    private val rules =
+        mapOf(
+            FIZZ_BUZZ to listOf(3, 5),
+            FIZZ_FOO to listOf(3, 7),
+            FIZZ_BOO to listOf(3, 11),
+            BUZZ_FOO to listOf(5, 7),
+            BUZZ_BOO to listOf(5, 11),
+            FOO_BOO to listOf(7, 11),
+            FIZZ to listOf(3),
+            BUZZ to listOf(5),
+            FOO to listOf(7),
+            BOO to listOf(11)
+        )
 
     fun fizzBuzz(input: Int): String {
         validateInput(input)
-        return when {
-            isFizzBuzz(input) -> FIZZ_BUZZ
-            isFizz(input) -> FIZZ
-            isBuzz(input) -> BUZZ
-            else -> input.toString()
-        }
+        return rules.entries.firstOrNull { entry -> entry.value.all { input % it == 0 } }?.key ?: input.toString()
     }
 
     fun fizzBuzzSequence(range: IntRange = 1..100) = range.map { fizzBuzz(it) }
@@ -24,10 +40,4 @@ class FizzBuzz {
         if (input < 1 || input > 100)
             throw InvalidNumberException()
     }
-
-    private fun isFizzBuzz(input: Int) = isFizz(input) && isBuzz(input)
-
-    private fun isBuzz(input: Int) = input % 5 == 0
-
-    private fun isFizz(input: Int) = input % 3 == 0
 }
--- src/test/kotlin/com/tdd/fizzbuzz/FizzBuzzTest.kt
+++ src/test/kotlin/com/tdd/fizzbuzz/FizzBuzzTest.kt
@@ -17,8 +17,72 @@ class FizzBuzzTest {
         @JvmStatic
         fun provideTestDataForSequence(): Stream<Arguments> {
             return Stream.of(
-                Arguments.of(1..20, listOf("1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz", "16", "17", "Fizz", "19", "Buzz")),
-                Arguments.of(15..50, listOf("FizzBuzz", "16", "17", "Fizz", "19", "Buzz", "Fizz", "22", "23", "Fizz", "Buzz", "26", "Fizz", "28", "29", "FizzBuzz", "31", "32", "Fizz", "34", "Buzz", "Fizz", "37", "38", "Fizz", "Buzz", "41", "Fizz", "43", "44", "FizzBuzz", "46", "47", "Fizz", "49", "Buzz"))
+                Arguments.of(
+                    1..20,
+                    listOf(
+                        "1",
+                        "2",
+                        "Fizz",
+                        "4",
+                        "Buzz",
+                        "Fizz",
+                        "Foo",
+                        "8",
+                        "Fizz",
+                        "Buzz",
+                        "Boo",
+                        "Fizz",
+                        "13",
+                        "Foo",
+                        "FizzBuzz",
+                        "16",
+                        "17",
+                        "Fizz",
+                        "19",
+                        "Buzz"
+                    )
+                ),
+                Arguments.of(
+                    15..50,
+                    listOf(
+                        "FizzBuzz",
+                        "16",
+                        "17",
+                        "Fizz",
+                        "19",
+                        "Buzz",
+                        "FizzFoo",
+                        "Boo",
+                        "23",
+                        "Fizz",
+                        "Buzz",
+                        "26",
+                        "Fizz",
+                        "Foo",
+                        "29",
+                        "FizzBuzz",
+                        "31",
+                        "32",
+                        "FizzBoo",
+                        "34",
+                        "BuzzFoo",
+                        "Fizz",
+                        "37",
+                        "38",
+                        "Fizz",
+                        "Buzz",
+                        "41",
+                        "FizzFoo",
+                        "43",
+                        "Boo",
+                        "FizzBuzz",
+                        "46",
+                        "47",
+                        "Fizz",
+                        "Foo",
+                        "Buzz"
+                    )
+                )
             )
         }
     }
@@ -33,7 +97,7 @@ class FizzBuzzTest {
     }
 
     @ParameterizedTest
-    @ValueSource(ints = [1, 2, 4, 7, 8, 86])
+    @ValueSource(ints = [1, 2, 4, 13, 8, 86])
     fun `should return number while input not multiples three or five`(input: Int) {
         //given
 
@@ -45,7 +109,7 @@ class FizzBuzzTest {
     }
 
     @ParameterizedTest
-    @ValueSource(ints = [3, 9, 6, 12, 18, 99])
+    @ValueSource(ints = [3, 9, 6, 12, 18, 96])
     fun `should return Fizz while input multiples of three`(input: Int) {
         //given
 
@@ -57,7 +121,7 @@ class FizzBuzzTest {
     }
 
     @ParameterizedTest
-    @ValueSource(ints = [5, 10, 20, 70, 100, 85])
+    @ValueSource(ints = [5, 10, 20, 100, 85])
     fun `should return Buzz while input multiples of five`(input: Int) {
         //given
 
@@ -67,19 +131,6 @@ class FizzBuzzTest {
         //then
         assertEquals(FizzBuzz.BUZZ, actual)
     }
-
-    @ParameterizedTest
-    @ValueSource(ints = [15, 30, 45, 60, 75, 90])
-    fun `should return FizzBuzz while input multiples of five and three`(input: Int) {
-        //given
-
-        //when
-        val actual = tested.fizzBuzz(input)
-
-        //then
-        assertEquals(FizzBuzz.FIZZ_BUZZ, actual)
-    }
-
 
     @Test
     fun `should generate FizzBuzz sequence from 1 to 100`() {
@@ -108,5 +159,102 @@ class FizzBuzzTest {
         //then
         assertEquals(arraySize, actual.size)
         assertEquals(expected, actual)
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [7, 14, 28, 49])
+    fun `should return Foo while input multiples of seven`(input: Int) {
+        //given
+
+        //when
+        val actual = tested.fizzBuzz(input)
+
+        //then
+        assertEquals(FizzBuzz.FOO, actual)
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [11, 22, 44, 88])
+    fun `should return Boo while input multiples of eleven`(input: Int) {
+        //given
+
+        //when
+        val actual = tested.fizzBuzz(input)
+
+        //then
+        assertEquals(FizzBuzz.BOO, actual)
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(ints = [15, 30, 45, 60, 75, 90])
+    fun `should return FizzBuzz while input multiples of five and three`(input: Int) {
+        //given
+
+        //when
+        val actual = tested.fizzBuzz(input)
+
+        //then
+        assertEquals(FizzBuzz.FIZZ_BUZZ, actual)
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [77])
+    fun `should return FooBoo while input multiples of seven and eleven`(input: Int) {
+        //given
+
+        //when
+        val actual = tested.fizzBuzz(input)
+
+        //then
+        assertEquals(FizzBuzz.FOO_BOO, actual)
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [21, 42, 63, 84])
+    fun `should return FizzFoo while input multiples of three and seven`(input: Int) {
+        //given
+
+        //when
+        val actual = tested.fizzBuzz(input)
+
+        //then
+        assertEquals(FizzBuzz.FIZZ_FOO, actual)
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [33, 66, 99])
+    fun `should return FizzBoo while input multiples of three and eleven`(input: Int) {
+        //given
+
+        //when
+        val actual = tested.fizzBuzz(input)
+
+        //then
+        assertEquals(FizzBuzz.FIZZ_BOO, actual)
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [35, 70])
+    fun `should return BuzzFoo while input multiples of five and seven`(input: Int) {
+        //given
+
+        //when
+        val actual = tested.fizzBuzz(input)
+
+        //then
+        assertEquals(FizzBuzz.BUZZ_FOO, actual)
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [55])
+    fun `should return BuzzBoo while input multiples of five and eleven`(input: Int) {
+        //given
+
+        //when
+        val actual = tested.fizzBuzz(input)
+
+        //then
+        assertEquals(FizzBuzz.BUZZ_BOO, actual)
     }
 }
